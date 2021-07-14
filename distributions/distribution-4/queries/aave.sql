0e5dcb424cc96799c7fad6868b0ad47844a533f7
Guillaume Felley <guillaume@felley.io>
2021-07-14 18:24:54 +0200

    Document and prepare queries for snapshot 4

--- distributions/distribution-4/queries/aave.sql
+++ distributions/distribution-4/queries/aave.sql
@@ -1,8 +1,8 @@
 -- Config 
-DECLARE StartDate DEFAULT TIMESTAMP("2021-06-21 12:50:00+00"); -- UTC date, Set it to when to start to distribute rewards
-DECLARE CutoffDate DEFAULT TIMESTAMP("2021-06-22 12:50:00+00"); -- UTC date, Set it to when to stop to distribute rewards
+DECLARE StartDate DEFAULT TIMESTAMP("2021-06-23 12:50:00+00"); -- UTC date, Set it to when to start to distribute rewards
+DECLARE CutoffDate DEFAULT TIMESTAMP("2021-07-15 12:50:00+00"); -- UTC date, Set it to when to stop to distribute rewards
 DECLARE BorrowAsset DEFAULT "0x03ab458634910aad20ef5f1c8ee96f1d6ac54919"; -- Asset to reward borrowers RAI 0x03ab458634910aad20ef5f1c8ee96f1d6ac54919 USDC 0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48
-DECLARE TokenOffered DEFAULT 100e18; -- Number of FLX to distribute in total
+DECLARE TokenOffered DEFAULT 1430e18; -- Number of FLX to distribute in total
 
 -- Constants
 DECLARE RewardRate DEFAULT TokenOffered / CAST(TIMESTAMP_DIFF(CutoffDate, StartDate, SECOND) AS BIGNUMERIC); -- FLX dsitributed per second
